13cb91a218f41fbed2aca9022739d1c0e29aafe2
Lance Wolfe <lwolfe@talentpath.com>
2022-01-28 16:28:36 -0500

    Demonstrate basic user state that persists on refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { UserStoreService } from './user-store.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'capstone-frontend';
+
+  constructor(
+    private cookieService: CookieService,
+    private userStore: UserStoreService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.userStore.currentUser$.subscribe((user) => {
+      if (!user) {
+        let cookie = this.cookieService.get('falcon.sid');
+        let userInCookie = JSON.parse(cookie).userId;
+        if (userInCookie) {
+          this.userService.fetchUserDetails(userInCookie);
+        }
+      }
+    });
+  }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserStoreService } from '../user-store.service';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  userToGreet: User | null = null;
 
-  constructor() { }
+  constructor(private userStore: UserStoreService) {}
 
   ngOnInit(): void {
+    this.userStore.currentUser$.subscribe((response) => {
+      this.userToGreet = response;
+    });
   }
-
 }
