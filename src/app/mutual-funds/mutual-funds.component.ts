5cec9509edb5b85c4ef53d2345841cc634ab82f7
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-02-01 09:59:24 -0600

    fix table

--- src/app/mutual-funds/mutual-funds.component.ts
+++ src/app/mutual-funds/mutual-funds.component.ts
@@ -14,6 +14,8 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 export class MutualFundsComponent implements OnInit {
   newArr: any[] = [];
 
+ 
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatPaginator) dataSource!: MatTableDataSource<MutualFund>;
   @ViewChild(MatSort) sort!: MatSort;
@@ -21,37 +23,36 @@ export class MutualFundsComponent implements OnInit {
   constructor(private mutualFundService: MutualFundsService,
               private _liveAnncouncer: LiveAnnouncer) {}
 
-  mutualFunds:MutualFund[] = [];
+  mutualFunds:any[] = [];
 
   ngOnInit(): void {
     this.mutualFundService.getMutualFunds().subscribe(payload => {
-      // this.mutualFunds = payload;
-      this.dataSource = new MatTableDataSource(payload);
+      this.mutualFunds = payload;
+      this.dataSource = new MatTableDataSource(this.mutualFunds);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
     })
   }
 
-  columns = [
-    {columnDef: 'fundName', header: 'Mutual Fund Name', sortBy: 'Sort by header', cell: (element: any) => `${element.fundName}`},
-    {columnDef: 'symbol', header: 'Symbol', sortBy: 'Sort by symbol', cell: (element: any) => `${element.symbol}`},
-    {columnDef: 'yTD', header: 'YTD',  sortBy: 'Sort by yTD', cell: (element: any) => `${element.yTD}`},
-    {columnDef: 'yearOne', header: 'Year 1', sortBy: 'Sort by yearOne',  cell: (element: any) => `${element.yearOne}`},
-    {columnDef: 'yearThree', header: 'Year 3', sortBy: 'Sort by yearThree',  cell: (element: any) => `${element.yearThree}`},
-    {columnDef: 'yearFive', header: 'Year 5', sortBy: 'Sort by yearFive',  cell: (element: any) => `${element.yearThree}`},
-    {columnDef: 'yearTen', header: 'Year 10', sortBy: 'Sort by yearTen',  cell: (element: any) => `${element.yearTen}`},
-    {columnDef: 'inceptionDate', header: 'Inception Date', sortBy: 'Sort by inceptionDate',  cell: (element: any) => `${element.inceptionDate}`},
-    {columnDef: 'inceptionRate', header: 'Inception Rate', sortBy: 'Sort by inceptionRate',  cell: (element: any) => `${element.inceptionRate}`},
-    {columnDef: 'expenseRatio', header: 'Expense Ratio', sortBy: 'Sort by expenseRatio',  cell: (element: any) => `${element.expenseRatio}`},
-    {columnDef: 'nAV', header: 'NAV', sortBy: 'Sort by nAV',  cell: (element: any) => `${element.nAV}`},
-    {columnDef: 'risk', header: 'Risk', sortBy: 'Sort by risk',  cell: (element: any) => `${element.risk}`},
-    {columnDef: 'minimum', header: 'Minimum', sortBy: 'Sort by minimum',  cell: (element: any) => `$ ${element.minimum}`},
-    //{columnDef: 'action', header: 'Minimum', sortBy: 'Sort by minimum',  cell: () => `<button>Text</button>`},
+ // columns = [
+  //  {columnDef: 'fundName', sortBy: 'Sort by header', cell: (element: any) => `${element.fundName}`},
+  //  {columnDef: 'symbol',  sortBy: 'Sort by symbol', cell: (element: any) => `${element.symbol}`},
+    // {columnDef: 'yTD',   sortBy: 'Sort by yTD', cell: (element: any) => `${element.yTD}`},
+    // {columnDef: 'yearOne',  sortBy: 'Sort by yearOne',  cell: (element: any) => `${element.yearOne}`},
+    // {columnDef: 'yearThree',  sortBy: 'Sort by yearThree',  cell: (element: any) => `${element.yearThree}`},
+    // {columnDef: 'yearFive',  sortBy: 'Sort by yearFive',  cell: (element: any) => `${element.yearThree}`},
+    // {columnDef: 'yearTen',  sortBy: 'Sort by yearTen',  cell: (element: any) => `${element.yearTen}`},
+ // {columnDef: 'inceptionDate',  sortBy: 'Sort by inceptionDate',  cell: (element: any) => `${element.inceptionDate}`},
+    // {columnDef: 'inceptionRate',  sortBy: 'Sort by inceptionRate',  cell: (element: any) => `${element.inceptionRate}`},
+    // {columnDef: 'expenseRatio', sortBy: 'Sort by expenseRatio',  cell: (element: any) => `${element.expenseRatio}`},
+    // {columnDef: 'nAV', sortBy: 'Sort by nAV',  cell: (element: any) => `${element.nAV}`},
+    // {columnDef: 'risk', sortBy: 'Sort by risk',  cell: (element: any) => `${element.risk}`},
+    // {columnDef: 'minimum', sortBy: 'Sort by minimum',  cell: (element: any) => `$ ${element.minimum}`},
+    // {columnDef: 'action'},
+ // ];
 
-
-  ];
-
-  displayedColumns = this.columns.map(c => c.columnDef);
+  // displayedColumns = this.columns.map(c => c.columnDef);
+  displayedColumns: string[] = ['fundName', 'symbol', 'inceptionDate', 'expenseRatio', "naV", "action"];
   // dataSource = this.mutualFunds;
 
   announceSortChange(sortState: Sort) {
